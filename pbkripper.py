2ee4fe8ebd4f085c5ea15f53a3b8c0336aed9b84
Kiril Staikov <kstaikov@gmail.com>
2018-01-14 00:09:42 -0500

    user can choose resolution

--- pbkripper.py
+++ pbkripper.py
@@ -3,6 +3,7 @@ import sys
 import requests
 import re
 from os.path import dirname
+from os import system
 
 def get_shows():
     return requests.get(
@@ -42,24 +43,36 @@ def ask_which_episode(available_episodes):
 
     return int(input(f'Which episode do you want? [0-{index-1}]: '))
 
-def get_video_url(available_episodes, index_to_get):
+def ask_which_resolution():
+    resolution_mapping = ['hls-2500k', 'hls-1080p']  # 720, 1080
+    print('Available Resolutions:\n=====================:\n[0]: 720p\n[1]: 1080p')
+    resolution_index = int(input('Which Resolution? [0-1]: '))
+    return resolution_mapping[resolution_index]
+
+
+def get_video_url(available_episodes, index_to_get, resolution):
     for key, values in available_episodes[index_to_get]['videos'].items():
-        if key.startswith('hls-2500k'):
+        if key.startswith(resolution):
             return values['url']
 
 def get_path_and_filename(video_url):
     r = requests.get(video_url, allow_redirects=False)
     location = r.headers['Location']
     manifest = requests.get(location).content.decode('utf-8')
-    match = re.findall(
+    match_1080 = re.findall(
+        '#EXT-X-STREAM.*RESOLUTION=(?P<resolution>1920x1080|1440x1080),.*\n(?P<filename>.*)\n',
+        manifest
+    )
+    match_720 = re.findall(
         '#EXT-X-STREAM.*RESOLUTION=(?P<resolution>1280x720|960x720),.*\n(?P<filename>.*)\n',
         manifest
     )
+    match = match_1080 or match_720
     filename = match[0][1]
     path = dirname(location)
     return path, filename
 
-def get_output_filename(filename, show_title, episode_title):
+def get_output_filename(filename, show_title, episode_title, resolution):
     print(f'playlist: {filename}')
     patterns = (
         r'\w+[a-zA-Z](?P<season>\d{1,2})(?P<episode>\d{2})[-_]ep',
@@ -78,7 +91,7 @@ def get_output_filename(filename, show_title, episode_title):
     # Prompt for season and episode to make sure it fits?
     season, episode = match.groups()
     season = season.zfill(2)
-    output = f'{show_title} - S{season}E{episode} - {episode_title}.ts'
+    output = f'{show_title} - S{season}E{episode} - {episode_title} - {resolution}.ts'
     output = output.replace('/', ' ')
 
     return output
@@ -111,10 +124,16 @@ if __name__ == '__main__':
     available_episodes = check_available_episodes(show_title)
     if not available_episodes:
         sys.exit(f'No episodes available for series: "{show_title}". Try another show next time.')
+    system('clear')
     index_to_get = ask_which_episode(available_episodes)
     episode_title = available_episodes[index_to_get]['title']
-    video_url = get_video_url(available_episodes, index_to_get)
+    system('clear')
+    resolution = ask_which_resolution()
+    video_url = get_video_url(available_episodes, index_to_get, resolution)
     path, filename = get_path_and_filename(video_url)
-    output = get_output_filename(filename, show_title, episode_title)
+    resolution_mapping = {'hls-2500k': '720p', 'hls-1080p': '1080p'}
+    output = get_output_filename(
+        filename, show_title, episode_title, resolution_mapping[resolution]
+    )
     playlist_files = get_video_playlist_files(path, filename)
     create_output_file(playlist_files, output)
